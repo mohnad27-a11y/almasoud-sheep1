5ffdba344ad9115176b41b5d5429a9292522b61e
mohnad27-a11y <mohnad27@gmail.com>
2026-08-29 18:47:08 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/almasoud/sheep/MainActivity.java
@@ -0,0 +1,26 @@
+package com.almasoud.sheep;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://mohnad27-a11y.github.io/almasoud-sheep1/");
+    }
+}
